8f3dd5ed055a7daa0d85c6481166e2c398c8e7d3
vKremenok <vkremenok@lleida.net>
2025-04-16 20:58:20 +0200

    modified:   package.json vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -25,6 +25,7 @@ const dotPathFixPlugin = () => ({
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), dotPathFixPlugin()],
+  base: '',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
